dbebe500cc8259b7278e89d4cb9f571c6b09bdd8
Bartek <golabkowy@gmail.com>
2017-01-31 20:24:28 +0100

    added challenge table sql create querry

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,4 +7,16 @@ create table USER (
   EMAIL varchar(256),
   ENABLED boolean,
   POINTS NUMBER
-);
+}
+
+create table CHALLENGE{
+  ID_CHALLENGE NUMBER,
+  NAME varchar(255),
+  DESCRIPTION varchar(255), //chuja nie varchar
+  STATUS varchar(255),
+  CATEGORY varchar(255),
+  POINTS varchar(255),
+  REWARD_TYPE varchar(255),
+  REWARD_QUANTITY varchar(255),
+  CREATOR_ID NUMBER
+};
