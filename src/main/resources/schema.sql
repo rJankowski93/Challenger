f1adf6ed8e26e619810f8d358aba39e402513d29
Bartek <golabkowy@gmail.com>
2017-02-01 22:06:39 +0100

    complete sql tables creation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,16 +7,44 @@ create table USER (
   EMAIL varchar(256),
   ENABLED boolean,
   POINTS NUMBER
-}
+);
 
-create table CHALLENGE{
-  ID_CHALLENGE NUMBER,
+create table USERS_USERS(
+  ID_USER NUMBER,
+  ID_FRIEND NUMBER
+);
+
+create table USER_ROLE(
+  ID_ROLE NUMBER auto_increment,
+  ID_USER NUMBER,
+  ROLE varchar(255)
+);
+
+create table USER_HAS_GROUP(
+  ID_USER NUMBER,
+  ID_GROUP NUMBER
+);
+
+create table CHALLENGE (
+  ID_CHALLENGE NUMBER auto_increment,
   NAME varchar(255),
-  DESCRIPTION varchar(255), //chuja nie varchar
+  DESCRIPTION varchar(255),
   STATUS varchar(255),
   CATEGORY varchar(255),
   POINTS varchar(255),
   REWARD_TYPE varchar(255),
   REWARD_QUANTITY varchar(255),
   CREATOR_ID NUMBER
-};
+);
+
+create table CHALLENGES_USERS(
+  ID_CHALLENGE NUMBER,
+  ID_USER NUMBER
+);
+
+create table GROUP(
+  ID_GROUP NUMBER auto_increment,
+  NAME varchar(45)
+);
+
+
