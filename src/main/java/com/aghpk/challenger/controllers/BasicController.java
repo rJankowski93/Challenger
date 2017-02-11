386a7b277ac4e5bc098810a38637180ca231c819
Rafal Jankowski <rafal.jankowski.93@gmail.com>
2017-02-11 23:01:24 +0100

    custom login page

--- src/main/java/com/aghpk/challenger/controllers/BasicController.java
+++ src/main/java/com/aghpk/challenger/controllers/BasicController.java
@@ -2,12 +2,15 @@ package com.aghpk.challenger.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BasicController {
 
-    @RequestMapping(value = "/login")
-    public String home() {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String home(@RequestParam(value = "error", required = false) String error,
+                       @RequestParam(value = "logout", required = false) String logout) {
         return "index.html";
     }
 }
