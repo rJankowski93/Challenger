ea552ded3f91beee29e816ecf00f0b580c0940e3
Rafal Jankowski <rafal.jankowski.93@gmail.com>
2017-02-16 09:50:02 +0100

    Basic login-panel view. Author: RafalJ, Reviewer: KarolK

--- src/main/resources/static/app/login-panel/login-panel.component.ts
+++ src/main/resources/static/app/login-panel/login-panel.component.ts
@@ -3,7 +3,8 @@ import {Component} from "@angular/core";
 @Component({
     moduleId: module.id,
     selector: 'login-panel',
-    templateUrl: './login-panel.component.html'
+    templateUrl: './login-panel.component.html',
+    styleUrls: ['login-panel.component.css']
 })
 export class LoginPanelComponent {
 
